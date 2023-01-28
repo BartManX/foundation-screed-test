beb543e40dd077619bc3afbb5ad691e041b80925
BartManX <adrianbardt@gmail.com>
2023-01-27 21:28:57 -0600

    1-28
    test

--- src/routes/appsettings.json.js
+++ src/routes/appsettings.json.js
@@ -8,11 +8,11 @@ export async function get() {
     
     return {
         body: {
-            SCD_ENVIRONMENT: process.env["SCD_ENVIRONMENT"] || "development",
-            SCD_POOL_NAME: process.env["SCD_POOL_NAME"] || "fresh screed mining pool",
-            SCD_POOL_FRONTEND_URL: process.env["SCD_POOL_FRONTEND_URL"] || "localhost",
-            SCD_FOUNDATION_API_BASE: process.env["SCD_FOUNDATION_API_BASE"] || '127.0.0.1:3001/api/v1',
-            SCD_STRATUM_URL: process.env["SCD_STRATUM_URL"] || "lottononce.com",
+            SCD_ENVIRONMENT: process.env["SCD_ENVIRONMENT"] || "production",
+            SCD_POOL_NAME: process.env["SCD_POOL_NAME"] || "kawpow.digital - clore pool",
+            SCD_POOL_FRONTEND_URL: process.env["SCD_POOL_FRONTEND_URL"] || "173.255.197.125",
+            SCD_FOUNDATION_API_BASE: process.env["SCD_FOUNDATION_API_BASE"] || 'http://209.126.5.210:2080/api/v1',
+            SCD_STRATUM_URL: process.env["SCD_STRATUM_URL"] || "209.126.5.210",
             SCD_POOL_CARD_TITLE: process.env["SCD_POOL_CARD_TITLE"] || "Pool",
             SCD_SUPPORT_EMAIL: process.env["SCD_SUPPORT_EMAIL"] || "support@example.com",
 
@@ -26,24 +26,24 @@ export async function get() {
             // 	        and add a config option to make pool solo or shared only
             SCD_POOL_SHARED: toBool(process.env["SCD_POOL_SHARED"]) || false,
 
-            SCD_DEFAULT_COIN_ENDPOINT: process.env["SCD_DEFAULT_COIN_ENDPOINT"] || "/Bitcoin",
+            SCD_DEFAULT_COIN_ENDPOINT: process.env["SCD_DEFAULT_COIN_ENDPOINT"] || "/solo-clore",
 
             // Whether to hide blocks card or not
             // Useful for solo pools, or pools just starting wishing to hide an empty
             // block list.
-            SCD_HIDE_BLOCKS_CARD: toBool(process.env["SCD_HIDE_BLOCKS_CARD"]) || true,
+            SCD_HIDE_BLOCKS_CARD: toBool(process.env["SCD_HIDE_BLOCKS_CARD"]) || false,
 
             // Default: BTC current block reward
-            SCD_BLOCK_REWARD: parseFloat(process.env["SCD_BLOCK_REWARD"]) || 6.25,
+            SCD_BLOCK_REWARD: parseFloat(process.env["SCD_BLOCK_REWARD"]) || 263,
 
             // Default: BTC, 10 minutes
-            SCD_BLOCK_TIME_IN_SECONDS: parseInt(process.env["SCD_BLOCK_TIME_IN_SECONDS"]) || 600,
+            SCD_BLOCK_TIME_IN_SECONDS: parseInt(process.env["SCD_BLOCK_TIME_IN_SECONDS"]) || 60,
             SCD_BLOCK_TIME_UNIT: process.env["SCD_BLOCK_TIME_UNIT"] || 'minutes',
             // Default: seconds to minutes in template
             SCD_BLOCK_TIME_UNIT_DIVIDER: parseInt(process.env["SCD_BLOCK_TIME_UNIT_DIVIDER"]) || 60,
 
-            SCD_BLOCK_EXPLORER_URL: process.env["SCD_BLOCK_EXPLORER_URL"] || 'https://blockchain.com/btc/block',
-            SCD_TX_EXPLORER_URL: process.env["SCD_TX_EXPLORER_URL"] || 'https://blockchain.com/btc/tx',
+            SCD_BLOCK_EXPLORER_URL: process.env["SCD_BLOCK_EXPLORER_URL"] || 'https://exploreblockchain.clore.ai/block',
+            SCD_TX_EXPLORER_URL: process.env["SCD_TX_EXPLORER_URL"] || 'https://exploreblockchain.clore.ai/tx',
 
             // Convert Hash to Petahash by dividing it by 1 trillion -->
             SCD_NETWORK_HASHRATE_DISPLAY_UNIT: process.env["SCD_NETWORK_HASHRATE_DISPLAY_UNIT"] || 'petahash',
